d8f5c2d0405dd1d40128d6b671fece971b82da5b
Grant Shipley <gshipley@gmail.com>
2016-06-09 10:26:30 -0400

    Added logging

--- src/main/java/com/openshift/service/DemoResource.java
+++ src/main/java/com/openshift/service/DemoResource.java
@@ -1,13 +1,16 @@
 package com.openshift.service;
 
+import com.openshift.helpers.Load;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.SecurityContext;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
-import com.openshift.helpers.Load;
 /**
  * A JAX-RS resource for exposing REST endpoints for Task manipulation
  */
@@ -16,12 +19,40 @@ public class DemoResource {
 
     @GET
     @Path("load/{seconds}")
-    @Produces({ "application/json" })
+    @Produces({"application/json"})
     public String generateLoad(@Context SecurityContext context, @PathParam("seconds") int seconds) {
-    	Load cpuLoad = new Load();
-    	cpuLoad.generateLoad(seconds*1000);
+        Load cpuLoad = new Load();
+        cpuLoad.generateLoad(seconds * 1000);
         return new String("Load generated for " + seconds + " seconds.");
     }
-    
+
+    @GET
+    @Path("log/info/{number}")
+    @Produces({"application/json"})
+    public String logInfo(@Context SecurityContext context, @PathParam("number") int number) {
+        Logger log = Logger.getLogger(DemoResource.class.getName());
+        log.log(Level.INFO, "OpenShift Information Log");
+        return new String("Added a log statement of type INFO");
+    }
+
+    @GET
+    @Path("log/error/{number}")
+    @Produces({"application/json"})
+    public String logSevere(@Context SecurityContext context, @PathParam("number") int number) {
+        Logger log = Logger.getLogger(DemoResource.class.getName());
+        log.log(Level.SEVERE, "OpenShift Information Log");
+        return new String("Added a log statement of type SEVERE");
+    }
+
+    @GET
+    @Path("log/warning/{number}")
+    @Produces({"application/json"})
+    public String logWarning(@Context SecurityContext context, @PathParam("number") int number) {
+        Logger log = Logger.getLogger(DemoResource.class.getName());
+        log.log(Level.WARNING, "OpenShift Information Log");
+        return new String("Added a log statement of type WARNING");
+    }
+
+
 
 }
