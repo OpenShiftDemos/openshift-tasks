09da27e8ca265c58b4d43571993ff8e9757eab75
Johnny Johnson <johnny2136@gmail.com>
2018-04-17 13:14:32 -0400

    Update Load.java
    InterruptedExceptions should never be ignored in the code, and simply logging the exception counts in this case as "ignoring". The throwing of the InterruptedException clears the interrupted state of the Thread, so if the exception is not handled properly the fact that the thread was interrupted will be lost. Instead, InterruptedExceptions should either be rethrown - immediately or after cleaning up the method's state - or the thread should be re-interrupted by calling Thread.interrupt() even if this is supposed to be a single-threaded application. Any other course of action risks delaying thread shutdown and loses the information that the thread was interrupted - probably without finishing its task.

--- src/main/java/com/openshift/helpers/Load.java
+++ src/main/java/com/openshift/helpers/Load.java
@@ -33,6 +33,8 @@ public class Load {
                 }
             } catch (InterruptedException e) {
                 e.printStackTrace();
+                 // Restore interrupted state...
+                Thread.currentThread().interrupt();
             }
         }
     }
